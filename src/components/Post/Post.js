06ff2e2ca6310a7d78cd969e70d66f47807c2b63
Nick Lemmon <nick.lemmon@gmail.com>
2018-02-07 20:47:00 -0500

    Fleshing out themes

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -4,14 +4,6 @@ import './Post.css';
 import PostHeader from './PostHeader.js';
 
 class Post extends Component {
-  focusTitle() {
-    this.title.focus()
-  }
-
-  componentDidMount() {
-    setTimeout( () => this.focusTitle(), 500 ) // focus delay helps with screen reader read/render delay
-  }
-
   render() {
     const title = this.props.title
     const author = this.props.author
@@ -19,16 +11,8 @@ class Post extends Component {
 
     return (
       <article className='Post'>
-        {/* negative `tabindex` for focusing purposes */}
-        <h1
-          className='Post-title'
-          ref={ ( title ) => { this.title = title; } }
-          tabIndex="-1"
-        >
-          { title }
-        </h1>
-
         <PostHeader 
+          title={ title }
           author={ author } 
           date={ date }
         />
--- src/components/Post/PostHeader.js
+++ src/components/Post/PostHeader.js
@@ -3,12 +3,29 @@ import React, { Component } from 'react';
 import './PostHeader.css';
 
 class PostHeader extends Component {
+  focusTitle() {
+    this.title.focus()
+  }
+
+  componentDidMount() {
+    setTimeout( () => this.focusTitle(), 250 ) // focus delay helps with screen reader read/render delay
+  }
+
   render() {
+    const title = this.props.title;
     const author = this.props.author;
     const date = this.props.date;
 
     return (
       <div className='PostHeader Theme--secondary'>
+        <h1
+          ref={ ( title ) => { this.title = title } }
+          className='PostHeader-title'
+          tabIndex="-1"
+        >
+          { title }
+        </h1>
+
         <img 
           className='PostHeader-avatar' 
           src='https://www.fillmurray.com/200/300' 
