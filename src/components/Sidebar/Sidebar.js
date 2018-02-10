97b95e6f0dd1c7e26d93dca80f4e75169564b7c6
Nick Lemmon <nick.lemmon@gmail.com>
2018-02-10 18:47:06 -0500

    Adding is-active styling us NavLink component

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Sidebar.css';
 
 class Sidebar extends Component {
   render() {
+    const activeClassName = 'is-active';
+
     let classNames = 'Sidebar Theme--tertiary';
   
     if ( this.props.classNames ) {
@@ -16,15 +18,33 @@ class Sidebar extends Component {
 
         <ul className='Sidebar-list'>
           <li className='Sidebar-listItem'>
-            <Link className="Sidebar-link" to="/post-1">Post 1</Link>
+            <NavLink 
+              activeClassName={ activeClassName } 
+              className="Sidebar-link" 
+              to="/post-1"
+            >
+              Post 1
+            </NavLink>
           </li>
 
           <li className='Sidebar-listItem'>
-            <Link className="Sidebar-link" to="/post-2">Post 2</Link>
+            <NavLink 
+              activeClassName={ activeClassName } 
+              className="Sidebar-link" 
+              to="/post-2"
+            >
+              Post 2
+            </NavLink>
           </li>
 
           <li className='Sidebar-listItem'>
-            <Link className="Sidebar-link" to="/post-3">Post 3</Link>
+            <NavLink 
+              activeClassName={ activeClassName } 
+              className="Sidebar-link" 
+              to="/post-3"
+            >
+              Post 3
+            </NavLink>
           </li>
         </ul>
       </nav>
