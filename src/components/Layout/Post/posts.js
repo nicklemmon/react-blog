b64a6d29ba388a99667ce500896174106a4c532b
Nick Lemmon <nick.lemmon@gmail.com>
2018-02-17 19:41:45 -0500

    Cleaning up some older styles

--- src/components/Layout/Post/posts.js
+++ src/components/Layout/Post/posts.js
@@ -6,9 +6,37 @@ const posts = [
     author: 'Bill Murray',
     date: '02/11/2018',
     content:
-  `## Herro.
-  A paragraph
-  ### Heading 3`
+  `## Reget fretum in tempore semihomines Phoebo obortas
+
+  Lorem markdownum vero nec prement pyra dixit levat quaerebatque facta, coniunx
+  circumsona, dies nubila, nunc. Manus tibi, **tum** tauri metallis, leto generis,
+  [si discede et](http://www.cumque-vultus.com/incedit). Adversa terreris in iussa
+  indignantia nati fames ope quod lacerans, et tacito. Flendo ulla sed sine
+  metuendus iram nequiquam faveatque movit.
+  
+  1. Suis rupit vobis
+  2. Iussa habentem
+  3. Clausa nisi virtus ut cum lactis
+  
+  ## Ora loci quisquis amicitiae addidit
+  
+  Spectat legit illam patrios potentem **fragorem instructamque** cecidere
+  protinus; amisit fuitque fuit, nam tu licuit Aethalion gutture illa. Lycurgum et
+  viscera, tale bella nomen formae *nunc tardus* praetentaque ferunt ventis
+  admirantibus primo! Successibus lignea tutus, ante amet iter tectam undas; arvo
+  in. Omnia finditque *iusta tandemque comes* postquam in terruit populos. Cadmus
+  **cura utque** Menephron **pone**, tactaeque et illic.
+  
+  > Fessos gravitate ut iunge passa, a primus cumque festis. Deponendique imago
+  > *questaque laceras foret*, minoris Austro transcribere multis: et! Cornua
+  > tamen, quam retia exemit artus, fuisset et spargimur videri dixit, ex.
+  > Ferentem septem at tamen amans si praeter pelagi, in quasque specieque valles
+  > Iovem fecisse hanc silvas ad ignibus.
+  
+  Quippe leve causa capellae forte patriaeque sacra, utque deducite qua tellure
+  bobus tamen reddidit. Vigoris hectora et fatali madidos carpit, **hanc** quarto
+  claudor deam pontus ardeat in habeo corpora ingemuit: advocat illa. Corpore igne
+  non urbem, nisi timorem. Iacentia ambitiosus, damna caelitibus grave.`
   },
   {
     id: 'post-02',
@@ -16,7 +44,25 @@ const posts = [
     subtitle: 'This is a subtitle',
     author: 'Bill Murray',
     date: '02/09/2018',
-    content: 'Herro again.'
+    content:
+  `## Ensem proxima ranas nam caelo dum pectora
+
+  Cerberei tura. Externum regnum possimne fallique hunc erat quo pulsat ex miratur
+  et. Pineta ac patriumque luna: staret credulus ab buxum nec fugante secutum, in!
+  Et **sinu priori** pugnando, foedumque orbem genas muris porrexerat, Lemnos.
+  
+  1. Erymanthidas mare
+  2. Fatur sinistra nullo Numidasque aura vara ut
+  3. Patriisque et corpore incursu labentia
+  4. Dianae latus illo
+  
+  Et cum pallor ramis heros laceri dicta admovit remorata venite limbo quidem
+  [minor oculosque](http://tuos.org/paulumque) Talia. Aperto et corpus et non post
+  **protexi poenae**, non his.
+  
+  Iuvenem hic arcum desierant bracchia rara antiquo Procrin. Me via terruit fronte
+  [Stygio](http://consensistismeorum.net/) erat telluris parte emittitque ignara!
+  Egesto fulmina, sol, sed longoque serius gemelliparae hanc iussit et diem sede.`
   },
   {
     id: 'post-03',
@@ -24,7 +70,25 @@ const posts = [
     subtitle: 'The best of all of the posts',
     author: 'Bill Murray',
     date: '02/07/2018',
-    content: 'Hey there.'
+    content:
+  `## Occasus subiti
+    
+  Lorem markdownum, *hoc in hunc* turba movens. Erat vir **mentibus aliquid
+  coepit** de aenis contulit videtur sacrum defluit. Deos idcirco se molles
+  mactare fera relictas quos: una vires late dixit pittheam et lapis! Transtulit
+  abductas quid, legerat virgo tenebant vidit. Facto quam tenebat utinam venerit
+  pericula perit, sub valuere poenam?
+  
+  - Labor lenis pependit Aonius maioris vincite stridente
+  - Meruisse saniemque
+  - Aurum vota paulatim vidit videbitur Cleonae ebore
+  - Sic Ceycis meum adsumus faciat et dentes
+  - Aequora harena concrevit vincere mando nemorisque videre
+  
+  Me me enim Troas nobis satis; fortisque minuit. Aere quod fati illa radices, in
+  nec postquam, est luctus ad coortis carina et Pylonque flumina. Cilix pars
+  veluti, voce ad pererrant erat *potenti manusque*. Neve scrobibus, [Achivae ipso
+  peragat](http://at-veneris.org/).`
   }
 ]
 
